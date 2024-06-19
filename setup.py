1738cc7f05db3ba6232d0298fac5d2f9c6af4fdc
unknown <kolya.strepetov@mail.ru>
2024-06-19 12:53:46 +0500

    Setup file uploaded

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Ssound',
+    version='0.2.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib'
+    ],
+    author='Nick',
+    description='A library for audio processing and analysis',
+    url='https://github.com/NikolayStrepetov/Ssound.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
